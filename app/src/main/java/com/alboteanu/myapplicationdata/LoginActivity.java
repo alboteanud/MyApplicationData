becb6a133dd4ea5a90e6233c1e90c3b717de1e41
albot <alboteanud@gmail.com>
2018-07-16 18:01:53 +0300

    added dummy contacts

---
+++ app/src/main/java/com/alboteanu/myapplicationdata/LoginActivity.java
@@ -0,0 +1,156 @@
+package com.alboteanu.myapplicationdata;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.alboteanu.myapplicationdata.others.Constants;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends BaseActivity {
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            goToMainActivity(LoginActivity.this);
+            return;
+        }
+
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.google_sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInGoogle();
+            }
+        });
+
+        if (isFirstUse())
+            signInGoogle();
+
+    }
+
+    private boolean isFirstUse() {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        boolean isFirstUse = sharedPreferences.getBoolean("1st_use", true);
+        if (isFirstUse){
+            sharedPreferences.edit().putBoolean("1st_use", false).apply();
+        }
+        return isFirstUse;
+    }
+
+    private void signInGoogle() {
+        showProgressDialog();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w("LoginActivity", "Google sign in failed", e);
+                hideProgressDialog();
+                Toast.makeText(LoginActivity.this, "Sign in error",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            hideProgressDialog();
+            Toast.makeText(LoginActivity.this, "Sign in error",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(@NonNull GoogleSignInAccount acct) {
+        Log.d("LoginActivity", "firebaseAuthWithGoogle: " + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        hideProgressDialog();
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LoginActivity", "signInWithCredential: success");
+                            goToMainActivity(LoginActivity.this);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LoginActivity", "signInWithCredential:failure", task.getException());
+//                                    updateUI(null);
+//                            onAuthFail(task.getException().getMessage());
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    ProgressDialog mProgressDialog;
+
+    void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+//            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    void goToMainActivity(Context context) {
+        Intent intentToMainActivity = new Intent(context, MainActivity.class);
+        intentToMainActivity.setAction(Constants.ACTION_UPDATE_LOCAL_CONTACTS);
+        intentToMainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intentToMainActivity);
+        finish();
+    }
+
+
+
+}
